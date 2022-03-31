55cc62349008c440e37b535fe7c8ba2860dafcb8
anthelme-lap <anthelmekoffi77@gmail.com>
2022-03-31 10:10:24 +0000

    ajout de sider

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\HomesliderRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(HomesliderRepository $homesliderRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig',[
+            'homesliders' => $homesliderRepository->findAll(),
+        ]);
     }
 }
